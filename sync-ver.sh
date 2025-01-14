60876a2dd7cc121baa5cf00b6700e7fb218bcba5
Depermitto <penciller@disroot.org>
2025-01-14 11:53:43 +0100

    Laid the foundation for the sync-ver.sh

--- sync-ver.sh
+++ sync-ver.sh
@@ -3,7 +3,6 @@
 
 # 1. Scan for git changes using gs:
 #   1.1 If they differ from local, save them to a variable
-#
 #   1.2 If not or a new file, do nothing
 # 2. Scan for local changes - compare local version of files to the git repo at ~/Gitlab/: 
 #   2.1 If they exist, save them to a variable
@@ -13,6 +12,15 @@
 #   3.2 Link local changes to the git storage
 
 
+local_config_files=["~/.config/nvim/init.vim" "~/.config/fish/config.fish" "~/.config/alacritty/alacritty.yml" "~/.bashrc" "~/.vimrc" "~/.config/qtile/config.py"]
+gitlab_config_files=["~/Gitlab/Nvim/init.vim" "~/Gitlab/Fish/config.fish" "~/Gitlab/Alacritty/alacritty.yml" "~/Gitlab/Bash/.bashrc" "~/Gitlab/Vim/.vimrc" "~/Gitlab/qtile/config.py"]
+
+
+function get_file_age() {
+    # Find time of last modification
+    return $(stat -c %y $1 | cut -d ' ' -f 2 | cut -d '.' -f 1) 
+}
+
+
 function git_changes() {
-    difference=$(gs -s | cut -d ' ' -f 2)
 }
