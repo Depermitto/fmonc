618f7d31d5e6f3d9ae96d89a77b8b46957b17ff7
Depermitto <penciller@disroot.org>
2025-01-14 11:53:43 +0100

    Updated qtile/config.py for laptops

--- qtile/config.py
+++ qtile/config.py
@@ -36,6 +36,7 @@ terminal = guess_terminal()
 keys = [
     # My custom keybindings
     Key([mod], "d", lazy.spawn("dmenu_run")),
+    Key([mod], "r", lazy.spawn("rofi -show drun")),
     Key([mod, "shift"], "d", lazy.spawn("networkmanager_dmenu")),
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on current window"),
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -43,23 +44,15 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
     Key(
         [mod, "shift"],
         "Return",
@@ -67,11 +60,39 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+    # LAPTOP KEYS
+
+    # Change volume
+    Key(
+        [], "XF86AudioRaiseVolume",
+        lazy.spawn('pamixer -i 5')
+    ),
+
+    Key(
+        [], "XF86AudioLowerVolume",
+        lazy.spawn('pamixer -d 5')
+    ),
+
+    Key(
+        [], "XF86AudioMute",
+        lazy.spawn('pamixer -t')
+    ),
+
+    # Change Brightness
+    Key(
+        [], "XF86MonBrightnessUp",
+        lazy.spawn('brightnessctl set +5%')
+    ),
+
+    Key(
+        [], "XF86MonBrightnessDown",
+        lazy.spawn('brightnessctl set 5%-')
+    ),
 ]
 
 
@@ -97,10 +118,6 @@ for i in groups:
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -133,7 +150,7 @@ layouts = [
 widget_defaults = dict(
     font='sans',
     fontsize=14,
-    padding=3,
+    padding=2,
 )
 extension_defaults = widget_defaults.copy()
 screens = [
@@ -141,92 +158,127 @@ screens = [
         bottom=bar.Bar(
             [
                 widget.GroupBox(
-                highlight_method='block',
-                urgent_alert_method='block',
-                urgent_text='FFFFFF',
-                this_current_screen_border=colors[1],
-                this_screen_border=colors[1],
-                other_current_screen_border=colors[3],
-                other_screen_border=colors[3],
-                inactive=colors[2],
-                disable_drag=True,
-                fontsize=15,
-                padding=7,
+                    highlight_method='block',
+                    urgent_alert_method='block',
+                    urgent_text='FFFFFF',
+                    this_current_screen_border=colors[1],
+                    this_screen_border=colors[1],
+                    other_current_screen_border=colors[3],
+                    other_screen_border=colors[3],
+                    inactive=colors[2],
+                    disable_drag=True,
+                    fontsize=15,
+                    padding=7,
                 ),
                 widget.TextBox(
-                '[',
-                padding=0,
+                    '[',
+                    padding=0,
                 ),
                 widget.CurrentLayout(
-                padding=0,
+                    padding=0,
                 ),
                 widget.TextBox(
-                ']',
-                padding=0,
+                    ']',
+                    padding=0,
                 ),
                 widget.WindowName(
-                max_chars=20,
-                padding=10,
+                    max_chars=20,
+                    padding=10,
                 ),
+
                 # CPU
                 widget.TextBox(
-                '',
-                fontsize=17,
-                padding=0,
+                    '',
+                    fontsize=17,
+                    padding=0,
                 ),
                 widget.CPU(
-                format='CPU: {load_percent}%'
+                    format='CPU: {load_percent}%'
                 ),
                 widget.TextBox(
-                padding=2
+                    padding=1
                 ),
+
                 # RAM Memory Usage
                 widget.TextBox(
-                '',
-                fontsize=17,
-                padding=0,
+                    '',
+                    fontsize=17,
+                    padding=0,
                 ),
                 widget.Memory(
-                format='MEM: {MemUsed:.1f}{mm}',
-                measure_mem='G'
+                    format='MEM: {MemUsed:.1f}{mm}',
+                    measure_mem='G'
                 ),
                 widget.TextBox(
-                padding=2
+                    padding=1
                 ),
-                # Calendar
+
+                # Volume
                 widget.TextBox(
-                '',
-                fontsize=17,
-                padding=0,
+                    '',
+                    fontsize=17,
+                    padding=0,
                 ),
-                widget.Clock(
-                format='%d/%m/%Y'
+                widget.PulseVolume(
+                    get_volume_command='pamixer --get-volume',
+                    update_interval=0,
                 ),
                 widget.TextBox(
-                padding=2
+                    padding=1
                 ),
+
+                # Brightness
+                widget.TextBox(
+                    '',
+                    fontsize=17,
+                    padding=0,
+                ),
+                widget.Backlight(
+                    backlight_name='intel_backlight',
+                    brightness_file='actual_brightness',
+                    update_interval=0,
+                ),
+                widget.TextBox(
+                    padding=1
+                ),
+
                 # Wifi
                 widget.TextBox(
-                '',
-                fontsize=17,
-                padding=2,
+                    '',
+                    fontsize=17,
+                    padding=0,
                 ),
                 widget.Wlan(
-                max_chars=12,
-                format='{essid}'
+                    max_chars=12,
+                    format='{essid}'
                 ),
                 widget.TextBox(
-                padding=2
+                    padding=1
                 ),
-                # Clock
+
+                # Calendar
                 widget.TextBox(
-                '',
-                fontsize=17,
-                padding=0,
+                    '',
+                    fontsize=17,
+                    padding=0,
                 ),
                 widget.Clock(
-                format='%H:%M:%S',
+                    format='%d/%m/%y'
                 ),
+                widget.TextBox(
+                    padding=1
+                ),
+
+                # Clock
+                widget.TextBox(
+                    '',
+                    fontsize=17,
+                    padding=0,
+                ),
+                widget.Clock(
+                    format='%H:%M:%S',
+                ),
+
             ],
             28,
             background=colors[0],
