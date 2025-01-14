fcf42da7240e84ae4197f33eda3e61c6d88748f2
Depermitto <penciller@disroot.org>
2025-01-14 11:53:42 +0100

    Fixed emacs

--- Doom/custom.el
+++ Doom/custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(good-scroll auto-complete neotree))
- '(warning-suppress-types '((org-element-cache))))
+ '(package-selected-packages
+   '(good-scroll s request-deferred pkg-info neotree dash auto-complete async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
